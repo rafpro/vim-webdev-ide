d466fcdd3eeb7f892280a277c0a01161fef9855a
Jose Elera Campana <jelera@gmail.com>
2012-01-16 01:39:18 -0600

    added symlinks for vim in clone-basic-plugins

--- clone-basic-plugins.sh
+++ clone-basic-plugins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+ln -s $HOME/dotfiles/vim $HOME/.vim
+ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/tyru/current-func-info.vim.git ~/.vim/bundle/current-func-info.vim
